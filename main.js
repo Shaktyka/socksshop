4e9cc89a95a57fb75d4023c88746bed087d32adf
Shaktyka <k-i-s-u-n-j-a@list.ru>
2019-05-02 14:52:24 +0600

    Добавление файлов. Начало ведения истории

---
+++ main.js
@@ -0,0 +1,251 @@
+// Компонент "Детали продукта"
+Vue.component('product-details', {
+  props: {
+    details: {
+      type: Array,
+      required: true
+    }
+  },
+  template: `
+    <ul>
+      <li v-for="item in details">{{ item }}</li>
+    </ul>
+  `
+});
+
+// Компонент для получения отзыва
+Vue.component('product-review', {
+  template: `
+    <form class="review-form" @submit.prevent="onSubmit">
+
+      <p v-if="errors.length">
+        <b>Please correct the following error(s):</b>
+        <ul>
+          <li v-for="error in errors">
+            {{ error }}
+          </li>
+        </ul>
+      </p>
+
+      <p>
+        <label for="name">Name:</label>
+        <input type="text" v-model="name" name="name" value="" tabindex="1" />
+      </p>
+
+      <p>
+        <label for="review">Review:</label>
+        <textarea v-model="review" cols="40" rows="8" name="review" id="review"></textarea>
+      </p>
+
+      <p>
+        <label for="rating">Rating:</label>
+        <select v-model="rating" name="rating" id="rating">
+          <option>5</option>
+          <option>4</option>
+          <option>3</option>
+          <option>2</option>
+          <option>1</option>
+        </select>
+      </p>
+
+      <p>
+        <input type="submit" value="Submit" />
+      </p>
+    </form>
+  `,
+  data() {
+    return {
+      name: null,
+      review: null,
+      rating: null,
+      errors: []
+    }
+  },
+  methods: {
+    onSubmit: function() {
+      if (this.name && this.review && this.rating) {
+        let productReview = {
+          name: this.name,
+          review: this.review,
+          rating: Number(this.rating)
+        }
+        this.$emit('review-submitted', productReview);
+        name: null;
+        review: null;
+        rating: null;
+      } else {
+        if (!this.name) {
+          this.errors.push("Name required.");
+        }
+        if (!this.review) {
+          this.errors.push("Review required.");
+        }
+        if (!this.rating) {
+          this.errors.push("Rating required.");
+        }
+      }
+    }
+  }
+});
+
+// Компонент "Продукт"
+Vue.component('product', {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true
+    }
+  },
+  template: `
+    <div class="product">
+
+      <div class="product-image">
+        <img :src="image" :alt="altText">
+      </div>
+
+      <div class="product-info">
+        <p>User is premium: {{ shipping }}</p>
+        <h2>{{ title }}</h2>
+        <p>{{ description }}</p>
+        <a :href="link">Buy</a>
+        <p v-if="inStock">In Stock</p>
+        <!-- <p v-else-if="inventory <= 10 && inventory > 0">Almost sold out!</p> -->
+        <p
+          v-else
+          :class="{ outOfStock: !inStock }"
+        >
+          Out Of Stock
+        </p>
+        <span v-show="onSale">{{ sale }}</span>
+
+        <!= Добавляем компонент product-details =>
+        <product-details :details="details"></product-details>
+
+        <div
+          class="color-box"
+          @mouseover="updateProduct(index)"
+          v-for="(variant, index) in variants"
+          :key="variant.variantId"
+          :style="{ backgroundColor: variant.variantColor}"
+        >
+        </div>
+        <ul>
+          <li v-for="size in sizes">{{ size }}</li>
+        </ul>
+        <button
+          v-on:click="addToCart"
+          :disabled="!inStock"
+          :class="{ disabledButton: !inStock }"
+        >
+          Add to cart
+        </button>
+        <button v-on:click="removeFromCart">Remove</button>
+      </div>
+
+      <div>
+        <h2>Reviews:</h2>
+        <p v-if="!reviews.length">There are no reviews yet.</p>
+        <ul>
+          <li v-for="review in reviews">
+            <p>{{ review.name }}</p>
+            <p>Rating: {{ review.rating }}</p>
+            <p>{{ review.review }}</p>
+          </li>
+        </ul>
+      </div>
+
+      <!= Добавляем компонент product-review =>
+      <product-review @review-submitted="addReview"></product-review>
+
+    </div>
+  `,
+  data() {
+    return {
+    product: "Socks",
+    brand: "Vue Mastery",
+    description: "A pair of warm, fuzzy socks",
+    selectedVariant: 0,
+    altText: "A pair of socks",
+    link: "socks.ru",
+    inventory: 15,
+    onSale: true,
+    details: ["80% cotton", "20% polyester", "Gender-newtral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: 'green',
+        variantImage: "./vmSocks-green.jpg",
+        variantQuantity: 10
+      },
+      {
+        variantId: 2235,
+        variantColor: 'blue',
+        variantImage: "./vmSocks-blue.jpg",
+        variantQuantity: 5
+      }
+    ],
+    sizes: ["35-36", "37-38", "39-40", "41-42"],
+    reviews: []
+    }
+  },
+  methods: {
+    addToCart: function() {
+      this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId);
+    },
+    removeFromCart: function() {
+      this.$emit('remove-from-cart', this.variants[this.selectedVariant].variantId);
+    },
+    updateProduct: function(index) {
+      this.selectedVariant = index;
+    },
+    addReview: function(productReview) {
+      this.reviews.push(productReview);
+    }
+  },
+  computed: {
+    title: function() {
+      return this.brand + ' ' + this.product;
+    },
+    image: function() {
+      return this.variants[this.selectedVariant].variantImage;
+    },
+    inStock: function() {
+      return this.variants[this.selectedVariant].variantQuantity
+    },
+    sale: function() {
+      if (this.onSale) {
+        return this.brand + ' ' + this.product + ' are on sale!';
+      } else {
+        return this.brand + ' ' + this.product + ' are not on sale';
+      }
+    },
+    shipping: function() {
+      if (this.premium) {
+        return "Free";
+      } else {
+        return 2.99;
+      }
+    }
+  }
+});
+
+
+const app = new Vue({
+  el: "#app",
+  data: {
+    premium: true,
+    cart: []
+  },
+  methods: {
+    updateCart(id) {
+      this.cart.push(id);
+    },
+    removeItem(id) {
+      for (let i = this.cart.length - 1; i >= 0; i--) {
+        if (this.cart[i] === id) {
+          this.cart.splice(i, 1);
+        }
+      }
+    }
+  }
+});
